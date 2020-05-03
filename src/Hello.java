874fefb13481d5660fe051289581e8ba911bf946
Padea <Padea@DESKTOP-LRREONA.attlocal.net>
2020-05-03 14:55:26 -0700

    Payton's test

--- src/Hello.java
+++ src/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, this is Duncan.");
         System.out.println("Hello, this is Kyle.");
+        System.out.println("Hello, this is Payton.");
         
     }
 }
